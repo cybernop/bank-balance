3737bada5c7498871c0519618d561c110c6be723
Alexander Schulze <cibernop@protonmail.com>
2022-08-27 12:54:37 +0200

    read all information from config

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-from bank_statement import parse
+from pathlib import Path
+
+import yaml
+
+from account import Account
+
+if __name__ == "__main__":
+    config = yaml.safe_load(Path("config.yml").read_text())
+
+    account = Account(config=config)
+    account.read_statements(config["statement_dir"])
+    pass
