8d30c9df8c21e956f91b575117cd7daf87d7504b
Alexander Schulze <cibernop@protonmail.com>
2022-09-03 10:44:55 +0200

    split down into functions

--- main.py
+++ main.py
@@ -1,27 +1,76 @@
 from datetime import date
 from pathlib import Path
+from typing import Dict
 
+import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as po
 import yaml
 
 from account_check.account import Account
 
-if __name__ == "__main__":
-    config = yaml.safe_load(Path("config.yml").read_text())
 
+def get_config():
+    return yaml.safe_load(Path("config.yml").read_text())
+
+
+def create_account(config) -> Account:
     account = Account(config=config)
     account.read_statements(config["statement_dir"])
+    return account
 
+
+def get_categories(account: Account, not_before: date = None) -> po.Figure:
+    data = account.categories_dataframe()
+
+    if not_before is not None:
+        data = filter_data(data, not_before)
+
+    figure = px.bar(
+        data,
+        x="month",
+        y="total",
+        color="category",
+        hover_name="category",
+    )
+
+    return figure
+
+
+def get_details(account: Account, not_before: date = None) -> po.Figure:
     data = account.dataframe()
-    first_date = date(day=1, month=7, year=2022)
-    data = data[data["month"] >= first_date]
 
-    fig = px.bar(
+    if not_before is not None:
+        data = filter_data(data, not_before)
+
+    figure = px.bar(
         data,
         x="month",
         y="amount",
         color="category",
         hover_name="description",
     )
-    fig.show()
+
+    return figure
+
+
+def filter_data(df: pd.DataFrame, not_before: date) -> pd.DataFrame:
+    return df[df["month"] >= not_before]
+
+
+def main():
+    config = get_config()
+    account = create_account(config)
+
+    not_before = date(day=1, month=7, year=2022)
+    figures = {
+        "Categories": get_categories(account, not_before),
+        "Details": get_details(account, not_before),
+    }
+
+    figures["Details"].show()
     input("any key to close...")
+
+
+if __name__ == "__main__":
+    main()
